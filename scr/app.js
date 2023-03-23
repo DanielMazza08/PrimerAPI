a64dd4da7b3e8d6a7aa16e0c72c970273ae44442
Cristian Mazza <cristianmazza08@gmail.com>
2023-03-23 19:34:19 -0300

    Primera api

---
+++ scr/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/api' , (req , res) => {
+    console.log('Paso por api');
+    res.json({mensaje: 'Hola Mundo', estado: 'OK'}).status(200)
+} );
+
+
+app.listen(3000 , () => {console.log('Escuchando en el puerto 3000')})
