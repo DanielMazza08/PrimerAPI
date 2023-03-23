66b6706a73715ab688b441ce090931dc66ffdb7e
Cristian Mazza <cristianmazza08@gmail.com>
2023-03-23 20:40:26 -0300

    Agregamos personas

--- scr/app.js
+++ scr/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const datos = require('../data/data.json');
 app.get('/api' , (req , res) => {
     console.log('Paso por api');
-    res.json({mensaje: 'Hola Mundo', estado: 'OK'}).status(200)
+    res.json(datos.personas).status(200)
 } );
 
 
 app.listen(3000 , () => {console.log('Escuchando en el puerto 3000')})
-console.log('Hola Mundo');
+
